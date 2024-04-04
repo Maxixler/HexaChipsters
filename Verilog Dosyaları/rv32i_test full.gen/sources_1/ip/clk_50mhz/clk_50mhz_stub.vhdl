8a388ccb34b6499d0ce16f2e17f4e076f1d1046f
Doğukan Biçer <dogukanbicer52@gmail.com>
2024-04-04 15:12:48 +0300

    Add files via upload

---
+++ Verilog Dosyaları/rv32i_test full.gen/sources_1/ip/clk_50mhz/clk_50mhz_stub.vhdl
@@ -0,0 +1,32 @@
+-- Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+-- Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2023.2 (win64) Build 4029153 Fri Oct 13 20:14:34 MDT 2023
+-- Date        : Sun Mar 17 15:59:43 2024
+-- Host        : Zenbook running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub -rename_top clk_50mhz -prefix
+--               clk_50mhz_ clk_50mhz_stub.vhdl
+-- Design      : clk_50mhz
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a100tcsg324-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity clk_50mhz is
+  Port ( 
+    clk_out1 : out STD_LOGIC;
+    reset : in STD_LOGIC;
+    locked : out STD_LOGIC;
+    clk_in1 : in STD_LOGIC
+  );
+
+end clk_50mhz;
+
+architecture stub of clk_50mhz is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk_out1,reset,locked,clk_in1";
+begin
+end;
